34a2a716fe1b296d79c54820cf5ded63476e3b04
sandraduarte <sandramduarte83@gmail.com>
2016-11-02 23:56:01 -0400

    updated friends and server

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+
+// ===============================================================================
+
+
+var friendsArray = [
+	{
+		name: 'Sandra',
+		photo: 'https://giphy.com/gifs/l41Yn7lKk2qhJPHA4',
+		scores: [5,5,5,5,5,5,5,5,5,5],
+
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
